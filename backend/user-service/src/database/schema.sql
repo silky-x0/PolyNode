295ba96e8e2bd9368521fe8f82459b13a02acebf
Akhil <10akhil.t@gmail.com>
2025-07-31 13:27:08 +0530

    added scehema for user-services

---
+++ backend/user-service/src/database/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role VARCHAR(50) DEFAULT "student",
+    email_verified BOOLEAN DEFAULT FALSE,
+    profile_picture_url VARCHAR(255),
+    bio TEXT,
+    timezone VARCHAR(50) DEFAULT 'UTC',
+    language VARCHAR(50) DEFAULT 'en',
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at TIMESTAMP NULL
+);
+
+
+CREATE TABLE user_sessions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    refresh_token_hash VARCHAR(255) UNIQUE NOT NULL,
+    device_info JSONB,
+    ip_address  INET,
+    last_used_at TIMESTAMP DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT NOW(),
+    expires_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE user_preferences (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) UNIQUE,
+    email_notifications BOOLEAN DEFAULT TRUE,
+    push_notifications BOOLEAN DEFAULT TRUE,
+    marketing_emails BOOLEAN DEFAULT TRUE,
+    theme VARCHAR(50) DEFAULT 'light',
+    notification_settings JSONB DEFAULT '{}',
+    privacy_settings JSONB DEFAULT '{}',
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
